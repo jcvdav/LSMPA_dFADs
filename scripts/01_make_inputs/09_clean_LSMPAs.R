c61c93b453a30257f50234f8a49f2f03d1687ebb
Juan Carlos Villaseñor-Derbez <jc_villasenor@miami.edu>
2024-12-12 13:07:52 -0500

    Updating MPAs

--- scripts/01_make_inputs/09_clean_LSMPAs.R
+++ scripts/01_make_inputs/09_clean_LSMPAs.R
@@ -26,10 +26,14 @@ pacman::p_load(
 # Load data --------------------------------------------------------------------
 list_of_lsmpas <- read_excel(here("raw_data", "list_LSMPAs.xlsx"), sheet = 1) %>% 
   clean_names() %>% 
-  select(wdpa_id, name, year_enforced) %>% 
-  filter(!wdpa_id == "220201")
+  select(wdpa_id, name, year_enforced)
 
-all_mpas <- list.files(path =  here("raw_data", "WDPA_WDOECM_Feb2023_Public_marine_shp"),
+# Downloaded from: https://www.protectedplanet.net/en/thematic-areas/marine-protected-areas
+# On Dec 12, 2024
+# UNEP-WCMC and IUCN (2024), Protected Planet: The World Database on Protected Areas (WDPA)
+# and World Database on Other Effective Area-based Conservation Measures (WD-OECM) [Online],
+# December 2024, Cambridge, UK: UNEP-WCMC and IUCN. Available at: www.protectedplanet.net.
+all_mpas <- list.files(path =  here("raw_data", "WDPA_WDOECM_Dec2024_Public_marine_shp"),
                        pattern = "polygons.shp",
                        full.names = T,
                        recursive = T) %>%
@@ -41,11 +45,14 @@ all_mpas <- list.files(path =  here("raw_data", "WDPA_WDOECM_Feb2023_Public_mari
 
 # Filter for relevant MPAs ------------------------------------------------------
 LSMPAs <- all_mpas %>% 
-  filter(wdpaid %in% list_of_lsmpas$wdpa_id | 
-           wdpa_pid %in% list_of_lsmpas$wdpa_id |
-           name %in% list_of_lsmpas$name) %>% 
-  st_make_valid() %>% 
-  filter(st_area(.) >= units::as_units(100000, "km2")) %>% 
+  filter(
+    wdpaid == "400011" |
+      wdpaid %in% list_of_lsmpas$wdpa_id |
+      wdpa_pid %in% list_of_lsmpas$wdpa_id |
+      name %in% list_of_lsmpas$name) %>%
+  st_break_antimeridian() |> 
+  st_make_valid() %>%
+  filter(st_area(.) >= units::as_units(100000, "km2")) %>%
   ms_simplify() %>%
   mutate(wdpaid = as.character(wdpaid)) %>%
   select(wdpaid, wdpa_pid, name, status_yr) %>% 
@@ -53,9 +60,10 @@ LSMPAs <- all_mpas %>%
   left_join(list_of_lsmpas %>% select(-name), by = join_by(wdpa_pid == wdpa_id)) %>% 
   mutate(year_enforced = coalesce(year_enforced.x, year_enforced.y),
          year_enforced = coalesce(year_enforced, status_yr)) %>% 
-  group_by(wdpaid, name, year_enforced) %>% 
+  group_by(wdpaid, name, year_enforced) %>%
   summarize(a = sum(1),
-            .groups = "drop") 
+            .groups = "drop") |>
+  filter(!wdpaid == "555512002") # This remove's PIPA's OLD polygon
 
 ## VISUALIZE ###################################################################
 
