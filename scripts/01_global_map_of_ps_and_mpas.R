3e9d5407ee4c69fc1cf1cae46fc0e03e33db41e1
jcvdav <juancarlos.villader@gmail.com>
2025-03-13 14:28:11 -0400

    Map of global dFAD effort finalized

--- scripts/01_global_map_of_ps_and_mpas.R
+++ scripts/01_global_map_of_ps_and_mpas.R
@@ -6,7 +6,7 @@
 # juancvd@stanford.edu
 # date
 #
-# Extract tuna PS effort layer at 1x1 degree from GFW and plot globally against large-scale MPAs
+# Map RFMO dfad effort
 #
 ################################################################################
 
@@ -41,46 +41,49 @@ theme_set(theme_linedraw(base_size = 9) +
                   legend.title = element_text(hjust = 0.5)))
 
 # Load data --------------------------------------------------------------------
-rfmo_ps_effort <- readRDS(file = here("processed_data", "annual_rfmo_effort_1deg.rds")) %>%
-  filter(between(year, 2013, 2021)) %>% 
-  select(-c(year, src)) %>% 
-  group_by(lat, lon) %>% 
-  summarize_all("mean", na.rm = T) %>% 
+# We'll summarize it as the mean annual effort ocurring between 2013 (first year of IOTC data) and 2021 (last year common to all)
+rfmo_ps_effort <- readRDS(file = here("processed_data", "annual_rfmo_effort_1deg.rds")) |>
+  filter(between(year, 2013, 2021),
+         sets_dfad >= 1) |> 
+  select(-c(year, src)) |> 
+  group_by(lat, lon) |> 
+  summarize_all("mean", na.rm = T) |> 
   filter(sets_dfad > 0)
 
-
+# Now build a raster
 rfmo_ps_effort_raster <- st_as_sf(x = rfmo_ps_effort,
                                   coords = c("lon", "lat"),
-                                  crs = 4326) %>% 
-  select(sets_dfad) %>% 
-  vect() %>% 
-  rasterize(x = .,
+                                  crs = 4326) |> 
+  select(sets_dfad) |> 
+  vect() |> 
+  rasterize(x = _,
             y = rast(ncol = 360, nrow = 180,
                      xmin = -180, xmax = 180,
                      resolution = 1, crs = "EPSG:4326"),
             field = "sets_dfad")
 
+rfmo_ps_intensity_raster <- (rfmo_ps_effort_raster / cellSize(rfmo_ps_effort_raster))
+
+target_resolution <- 100000
+rfmo_ps_effort_raster_reproj <- project(rfmo_ps_intensity_raster, proj, res = target_resolution, method = "bilinear") * (target_resolution ^ 2)
+
+# Check that values are relatively the same
 sum(values(rfmo_ps_effort_raster), na.rm = T)
-
-rfmo_ps_intensity_raster <- (rfmo_ps_effort_raster / cellSize(rfmo_ps_effort_raster)) %>%
-  project(proj, res = 100000, method = "bilinear")
-
-rfmo_ps_effort_raster_reproj <- rfmo_ps_intensity_raster * (100000 ^ 2)
-
 sum(values(rfmo_ps_effort_raster_reproj), na.rm = T)
 
-coast <- ne_countries(scale = "small") %>% 
-  st_break_antimeridian(lon_0 = center) %>% 
+coast <- ne_countries(scale = "medium") |> 
+  st_break_antimeridian(lon_0 = center) |> 
+  st_segmentize(dfMaxLength = 1000) |> 
   st_transform(proj)
   
-mpas <- st_read(here("processed_data", "selected_LSMPAs_viz.gpkg")) %>% 
-  st_break_antimeridian(lon_0 = center) %>% 
+mpas <- st_read(here("processed_data", "selected_LSMPAs_viz.gpkg")) |> 
+  st_break_antimeridian(lon_0 = center) |> 
   st_transform(proj)
 
-buffered_mpas <- mpas %>% 
-  st_buffer(dist = units::as_units(200, "nautical_miles")) %>% 
-  st_union() %>% 
-  st_as_sf() %>% 
+buffered_mpas <- mpas |> 
+  st_buffer(dist = units::as_units(200, "nautical_miles")) |> 
+  st_union() |> 
+  st_as_sf() |> 
   st_make_valid()
 
 ## VISUALIZE ###################################################################
@@ -88,16 +91,17 @@ buffered_mpas <- mpas %>%
 # X ----------------------------------------------------------------------------
 # Mean annual reported dFAD fishing intensity (2013-2021). Data from WCPFC, IATTC, ICCAT, and IOTC
 main_map <- ggplot() +
-  geom_spatraster(data = log(rfmo_ps_effort_raster_reproj)) +
-  scale_fill_viridis_c(option = "mako",
-                       name = "Effort [log(num. sets)]",
+  geom_spatraster(data = log10(rfmo_ps_effort_raster_reproj)) +
+  scale_fill_viridis_c(option = "viridis",
+                       name = expression(Effort (log[10](num.~sets~per~10000~km^2))),
                        guide = guide_colorbar(frame.colour = "black",
                                               ticks.colour = "black"),
                        na.value = "transparent") +
   theme(legend.title.position = "top") +
+  theme(panel.grid.major = element_blank()) +
   geom_sf(data = mpas,
           fill = scales::muted("red"),
-          color = "black",
+          color = scales::muted("red"),
           linewidth = 0.1) +
   geom_sf(data = buffered_mpas,
           fill = "transparent",
@@ -106,92 +110,19 @@ main_map <- ggplot() +
   geom_sf(data = coast,
           fill = "transparent",
           color = "black",
-          linewidth = 0.5) +
+          linewidth = 0.2) +
   geom_sf(data = coast,
           fill = "gray90",
           color = "gray90",
-          linewidth = 0) +
+          linewidth = 0.1) +
   scale_x_continuous(expand = c(0, 0.1)) +
   scale_y_continuous(expand = c(0, 0.1))
 
+# main_map
+
 ## EXPORT ######################################################################
 ggsave(plot = main_map,
-       filename = "global_map_of_dfad_effort_and_mpas.pdf",
+       filename = here("results", "figs", "global_map_of_dfad_effort_and_mpas.pdf"),
        units = "cm",
        width = 9.2,
        height = 6)
-
-
-
-#### EXTRAS
-dFAD_tracks <- read_csv(here("raw_data", "data-fads20122018-imzilen2022.csv"))
-
-gfw_ps_effort <- readRDS(here("processed_data", "annual_gfw_tuna_ps_effort_1deg.rds")) %>% 
-  select(-year) %>% 
-  group_by(lat, lon) %>% 
-  summarize_all("sum", na.rm = T) %>% 
-  filter(n_vessels_p3m1 > 0)
-
-tracks_map <- ggplot() +
-  geom_tile(data = dFAD_tracks,
-            aes(x = lon, y = lat, fill = log(num))) +
-  scale_fill_viridis_c(option = "viridis",
-                       name = "Reported dFAD track intensity [log(num. tracks)]",
-                       guide = guide_colorbar(frame.colour = "black",
-                                              ticks.colour = "black")) +
-  theme(legend.title.position = "top") +
-  geom_sf(data = mpas,
-          fill = scales::muted("red"),
-          color = "black",
-          linewidth = 0.1) +
-  geom_sf(data = buffered_mpas,
-          fill = "transparent",
-          color = "black",
-          linewidth = 0.1) +
-  geom_sf(data = coast,
-          fill = "transparent",
-          color = "black",
-          linewidth = 1) +
-  geom_sf(data = coast,
-          fill = "gray50",
-          color = "gray50",
-          linewidth = 0.1) +
-  scale_x_continuous(expand = c(0, 0)) +
-  scale_y_continuous(expand = c(0, 0))
-
-gfw_map <- ggplot() +
-  geom_tile(data = gfw_ps_effort,
-            aes(x = lon, y = lat, fill = log(fishing_hours_sunrise_p3m1))) +
-  scale_fill_viridis_c(option = "inferno",
-                       name = "Inferred dFAD fishing effort [log(num. sets)]",
-                       guide = guide_colorbar(frame.colour = "black",
-                                              ticks.colour = "black")) +
-  theme(legend.title.position = "top") +
-  geom_sf(data = mpas,
-          fill = scales::muted("red"),
-          color = "black",
-          linewidth = 0.1) +
-  geom_sf(data = buffered_mpas,
-          fill = "transparent",
-          color = "black",
-          linewidth = 0.5) +
-  geom_sf(data = coast,
-          fill = "transparent",
-          color = "black",
-          linewidth = 1) +
-  geom_sf(data = coast,
-          fill = "gray50",
-          color = "gray50",
-          linewidth = 0.1) +
-  scale_x_continuous(expand = c(0, 0)) +
-  scale_y_continuous(expand = c(0, 0))
-
-tracks_and_gfw_map <- plot_grid(tracks_map, gfw_map,
-                                ncol = 1,
-                                labels = "AUTO")
-ggsave(plot = tracks_and_gfw_map,
-       filename = "global_map_of_tracks_gfw_and_mpas.pdf",
-       units = "cm",
-       width = 12.1,
-       height = 16)
-
